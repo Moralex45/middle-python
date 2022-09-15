69eb4454c0c29d6acd335e5f6ce2b8ca671f16c6
Aleksandr Morozov <moralex45@gmail.com>
2022-09-15 12:45:45 +0300

    commit data for tests

--- authapi-service/src/extentions.py
+++ authapi-service/src/extentions.py
@@ -2,16 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
-from core.config import get_settings_instance
+# from core.config import get_settings_instance
 
-engine = create_engine(
-    get_settings_instance().postgres_connection_url, 
-    echo=True
-)
+# engine = create_engine(
+#     get_settings_instance().postgres_connection_url, 
+#     echo=True
+# )
 
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+# db_session = scoped_session(sessionmaker(autocommit=False,
+#                                          autoflush=False,
+#                                          bind=engine))
 
 Base = declarative_base()
-Base.query = db_session.query_property()
+# Base.query = db_session.query_property()
