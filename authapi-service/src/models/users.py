319fac89d497f15433a277d8d02c0d4545ec5b6a
Aleksandr Morozov <moralex45@gmail.com>
2022-09-15 17:39:35 +0300

    Password generation and verification function

--- authapi-service/src/models/users.py
+++ authapi-service/src/models/users.py
@@ -3,10 +3,16 @@ import datetime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.dialects.postgresql import INET
 from sqlalchemy.ext.hybrid import hybrid_property
-from werkzeug.security import check_password_hash, generate_password_hash
+import hashlib
+import bcrypt
 
 from sqlalchemy import Column, VARCHAR, ForeignKey, TEXT, TIMESTAMP, BOOLEAN
 from models.base import BaseModel
+
+
+class PasswordConstants:
+    algorithm = 'sha256'
+    iterations = 100000
 
 
 class User(BaseModel):
@@ -27,15 +33,30 @@ class User(BaseModel):
 
     @password.setter
     def password(self, value):
-        self.pwd_hash = generate_password_hash(value)
+        salt = bcrypt.gensalt()
+        self.pwd_hash = hashlib.pbkdf2_hmac(
+            PasswordConstants.algorithm,
+            value.encode('utf-8'),
+            salt,
+            PasswordConstants.iterations
+            ) + b'$' + salt
 
     def check_password(self, value):
-        return check_password_hash(self.pwd_hash, value)
+        received_salt = self.pwd_hash[-29:]
+        received_pwd_hash = hashlib.pbkdf2_hmac(
+            PasswordConstants.algorithm,
+            value.encode('utf-8'),
+            received_salt,
+            PasswordConstants.iterations
+            ) + b'$' + received_salt
+
+        return self.pwd_hash == received_pwd_hash
+
 
 class UserData(BaseModel):
     __tablename__ = 'users_data'
 
-    user_id = Column(ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     first_name = Column(TEXT())
     last_name = Column(TEXT())
     email = Column(TEXT())
@@ -44,14 +65,15 @@ class UserData(BaseModel):
     def __repr__(self):
         return f'{self.first_name} {self.last_name}'
 
+
 class AuthHistory(BaseModel):
     __tablename__ = 'auth_history'
 
-    user_id = Column(UUID(as_uuid=True),ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     ip = Column(INET())
     user_agent = Column(TEXT())
     date_start = Column(TIMESTAMP())
-    date_end= Column(TIMESTAMP())
+    date_end = Column(TIMESTAMP())
 
     def __repr__(self):
         return f'{self.ip} {self.user_agent}'
