c2291afaffafcabc5628da634feaeecb6eb9cbd1
Ruslan Filichkin <ruslan.filichkin@gmail.com>
2022-09-18 21:42:46 +0300

    Implement test cover for get filtered user role

--- authapi-service/src/api/v1/crud/user_role.py
+++ authapi-service/src/api/v1/crud/user_role.py
@@ -1,7 +1,9 @@
 import uuid
 from http import HTTPStatus
 
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
+import orjson
+from pydantic.json import pydantic_encoder
 
 from db.services.user_role import UserRoleService
 from core.out_models.user_role import UserRole as OutUserRole
@@ -25,21 +27,21 @@ def get_role_permission(user_role_id: uuid.UUID):
     return Response(response_body, status=response_status, mimetype='application/json')
 
 
-# @blueprint.route('/', methods=['GET'])
-# def get_roles_permissions():
-#     response_body = ''
-#     response_status = HTTPStatus.OK
-#
-#     role_id: uuid.UUID | None = request.args.get('role_id', default=None, type=uuid.UUID)
-#     if role_id is None:
-#         response_status = HTTPStatus.BAD_REQUEST
-#
-#         return Response(response_body, status=response_status, mimetype='application/json')
-#
-#     db_role_permissions = RolePermissionService.get_filtered(role_id)
-#     role_permissions = orjson.dumps(
-#         [OutRolePermission.from_orm(db_role_permission) for db_role_permission in db_role_permissions],
-#         default=pydantic_encoder)
-#     response_body = role_permissions
-#
-#     return Response(response_body, status=response_status, mimetype='application/json')
+@blueprint.route('/', methods=['GET'])
+def get_roles_permissions():
+    response_body = ''
+    response_status = HTTPStatus.OK
+
+    user_id: uuid.UUID | None = request.args.get('user_id', default=None, type=uuid.UUID)
+    if user_id is None:
+        response_status = HTTPStatus.BAD_REQUEST
+
+        return Response(response_body, status=response_status, mimetype='application/json')
+
+    db_users_roles = UserRoleService.get_filtered(user_id)
+    role_permissions = orjson.dumps(
+        [OutUserRole.from_orm(db_user_role) for db_user_role in db_users_roles],
+        default=pydantic_encoder)
+    response_body = role_permissions
+
+    return Response(response_body, status=response_status, mimetype='application/json')
--- authapi-service/tests/functional/src/test_crud_user_role.py
+++ authapi-service/tests/functional/src/test_crud_user_role.py
@@ -2,7 +2,7 @@ import uuid
 
 import pytest
 
-from functional.testdata.database_fake_data import roles_permissions, roles, permissions, users_roles
+from functional.testdata.database_fake_data import roles_permissions, roles, permissions, users_roles, users
 from http import HTTPStatus
 
 
@@ -21,3 +21,32 @@ def test_scrap_non_existing_user_role(flask_test_client, clean_database, generat
     assert response.status_code == HTTPStatus.NO_CONTENT
     assert response.text == ''
 
+
+@pytest.mark.parametrize(
+    'user',
+    [user for user in users])
+def test_scrap_existing_user_role_filtered_by_user_id(flask_test_client,
+                                                      clean_database,
+                                                      generate_users_roles,
+                                                      user):
+    response = flask_test_client.get(f'/api/v1/user_role/?user_id={user["id"]}')
+    assert response.status_code == HTTPStatus.OK
+    assert response.is_json
+    assert response.json == list(filter(lambda o: o['user_id'] == user['id'], users_roles))
+
+
+def test_scrap_non_existing_user_role_permissions_filtered_by_role_id(flask_test_client,
+                                                                      clean_database,
+                                                                      generate_users_roles):
+    response = flask_test_client.get(f'/api/v1/user_role/?user_id={uuid.uuid4()}')
+    assert response.status_code == HTTPStatus.OK
+    assert response.is_json
+    assert response.json == []
+
+
+def test_scrap_user_role_filtered_by_inappropriate_role_id_format(flask_test_client,
+                                                                  clean_database,
+                                                                  generate_users_roles):
+    response = flask_test_client.get(f'/api/v1/user_role/?user_id={str(uuid.uuid4())[-1:]}')
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.text == ''
