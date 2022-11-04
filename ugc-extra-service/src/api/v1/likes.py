a0bacf916aa28178e2022d2b1baeca0e56fa4338
Ruslan Filichkin <ruslan.filichkin@gmail.com>
2022-11-04 14:48:15 +0300

    Implement average mark computing endpoint

--- ugc-extra-service/src/api/v1/likes.py
+++ ugc-extra-service/src/api/v1/likes.py
@@ -14,23 +14,24 @@ router = fastapi.APIRouter(prefix='/api/v1/likes')
 
 
 @router.post('/',
-             response_model=inner_likes_models.Like,
+             response_model=http_likes_models.Like,
              status_code=fastapi.status.HTTP_201_CREATED,
              description='Создание новой оценки кинопроизведения в системе',
              summary='Endpoint позволяет создать новую оценку кинопроизведения в системе',
              tags=['Лайки'],
              dependencies=[fastapi.Depends(verify_auth_tokens)])
 async def create_like(
-        http_like: http_likes_models.Like,
+        http_like: http_likes_models.LikeCreation,
         like_repository: AsyncMongoDBLikeRepository = fastapi.Depends(get_like_repository),
-) -> inner_likes_models.Like:
+) -> http_likes_models.Like:
     try:
-        return await like_repository.create_like(
+        like = await like_repository.create_like(
             http_like.user_id, http_like.movie_id, http_like.mark, http_like.device_fingerprint,
         )
+        return http_likes_models.Like(**like.to_dict())
 
     except repositories_exception.DataAlreadyExistsError:
-        raise fastapi.HTTPException(status_code=400, detail='Like already exist')
+        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_400_BAD_REQUEST, detail='Like already exist')
 
 
 @router.delete('/',
@@ -47,22 +48,43 @@ async def delete_like(
         await like_repository.delete_like(user_id, movie_id)
 
     except repositories_exception.DataDoesNotExistError:
-        raise fastapi.HTTPException(status_code=400, detail='Like does not exist')
+        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_400_BAD_REQUEST, detail='Like does not exist')
 
 
 @router.patch('/',
-              response_model=inner_likes_models.Like,
+              response_model=http_likes_models.Like,
               status_code=fastapi.status.HTTP_200_OK,
               description='Изменение оценки кинопроизведения в системе',
               summary='Endpoint позволяет изменить оценку кинопроизведения в системе',
               tags=['Лайки'],
               dependencies=[fastapi.Depends(verify_auth_tokens)])
 async def update_like(
-        user_id: uuid.UUID, movie_id: uuid.UUID, mark: int,
+        user_id: uuid.UUID, movie_id: uuid.UUID, mark: int = fastapi.Query(ge=0.0, le=10.0),
         like_repository: AsyncMongoDBLikeRepository = fastapi.Depends(get_like_repository),
-) -> inner_likes_models.Like:
+) -> http_likes_models.Like:
     like = await like_repository.get_like(user_id, movie_id)
     if like is None:
-        raise fastapi.HTTPException(status_code=400, detail='Like does not exist')
+        raise fastapi.HTTPException(status_code=fastapi.status.HTTP_400_BAD_REQUEST, detail='Like does not exist')
     await like_repository.delete_like(user_id, movie_id)
-    return await like_repository.create_like(like.user_id, like.movie_id, mark, like.device_fingerprint, _id=like.id)
+    like = await like_repository.create_like(like.user_id, like.movie_id, mark, like.device_fingerprint, _id=like.id)
+    return http_likes_models.Like(**like.to_dict())
+
+
+@router.post('/average_mark',
+             response_model=http_likes_models.AverageLikeMarkByFilm,
+             status_code=fastapi.status.HTTP_200_OK,
+             description='Просмотр средней оценки кинопроизведения в системе',
+             summary='Endpoint позволяет просмотреть среднюю оценку кинопроизведения в системе',
+             tags=['Лайки'],
+             dependencies=[fastapi.Depends(verify_auth_tokens)])
+async def count_average_mark(
+        movie_id: uuid.UUID,
+        like_repository: AsyncMongoDBLikeRepository = fastapi.Depends(get_like_repository),
+) -> http_likes_models.AverageLikeMarkByFilm:
+    movie_mark = await like_repository.get_average_movie_mark(movie_id)
+    marks_amount = await like_repository.get_movie_likes_amount(movie_id)
+    if marks_amount == 0 or movie_mark is None:
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_417_EXPECTATION_FAILED, detail='Like does not exist'
+        )
+    return http_likes_models.AverageLikeMarkByFilm(mark=movie_mark, amount=marks_amount)
--- ugc-extra-service/src/models/http/events/like.py
+++ ugc-extra-service/src/models/http/events/like.py
@@ -1,10 +1,25 @@
 import uuid
 
+from pydantic import Field
+
 from src.models.inner.base import Base
 
 
-class Like(Base):
+class LikeCreation(Base):
     user_id: uuid.UUID
     movie_id: uuid.UUID
     mark: int
     device_fingerprint: str
+
+
+class Like(Base):
+    id: uuid.UUID = Field(..., alias='_id')  # noqa
+    user_id: uuid.UUID
+    movie_id: uuid.UUID
+    mark: int
+    device_fingerprint: str
+
+
+class AverageLikeMarkByFilm(Base):
+    mark: float
+    amount: int
