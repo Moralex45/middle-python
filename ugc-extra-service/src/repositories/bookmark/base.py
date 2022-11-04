558325f7dc15493bfdbb418b48959160b8996c6f
Ruslan Filichkin <ruslan.filichkin@gmail.com>
2022-11-04 18:53:49 +0300

    Implement bookmarks repository

--- ugc-extra-service/src/repositories/bookmark/base.py
+++ ugc-extra-service/src/repositories/bookmark/base.py
@@ -8,13 +8,17 @@ from src.models.inner.events.bookmark import Bookmark
 
 class AsyncBookmarkRepositoryProtocol(ABC):
     @abstractmethod
-    async def get_users_bookmark(self, user_id: uuid.UUID) -> list[Bookmark]:
+    async def get_user_bookmarks(self, user_id: uuid.UUID) -> list[Bookmark]:
         raise NotImplementedError
 
     @abstractmethod
-    async def create_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID) -> Bookmark:
+    async def create_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID, _id: uuid.UUID = None) -> Bookmark:
         raise NotImplementedError
 
     @abstractmethod
     async def delete_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID) -> Bookmark | None:
         raise NotImplementedError
--- ugc-extra-service/src/repositories/bookmark/mongodb.py
+++ ugc-extra-service/src/repositories/bookmark/mongodb.py
@@ -19,11 +19,45 @@ class AsyncMongoDBBookmarkRepository(AsyncBookmarkRepositoryProtocol):
             project_config.get_settings().mongodb_settings.mongodb_bookmark_collection
         ]
 
-    async def get_users_bookmark(self, user_id: uuid.UUID) -> list[Bookmark]:
-        pass
+    async def get_user_bookmarks(self, user_id: uuid.UUID) -> list[Bookmark]:
+        query = {'user_id': str(user_id)}
+        documents = await self.collection.find(query).to_list(await self.__count_documents(query))
+        return [Bookmark(**document) for document in documents]
 
-    async def create_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID) -> Bookmark:
-        pass
+    async def create_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID, _id: uuid.UUID = None) -> Bookmark:
+        """
+        Raises:
+            repositories_exception.DataAlreadyExistsError: on inability to create like
+
+        """
+        bookmark = Bookmark(
+            _id=uuid.uuid4() if _id is None else _id,
+            user_id=user_id,
+            movie_id=movie_id,
+        )
+        if await self.get_bookmark(bookmark.user_id, bookmark.movie_id) is not None:
+            raise repositories_exception.DataAlreadyExistsError()
+        await self.collection.insert_one(bookmark.to_dict())
+        return bookmark
 
     async def delete_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID) -> None:
-        pass
+        """
+        Raises:
+            repositories_exception.DataDoesNotExistError: on inability to delete like
+
+        """
+        if await self.get_bookmark(user_id, movie_id) is None:
+            raise repositories_exception.DataDoesNotExistError()
+        query = {'user_id': str(user_id), 'movie_id': str(movie_id)}
+        await self.collection.delete_one(query)
+
+    async def get_bookmark(self, user_id: uuid.UUID, movie_id: uuid.UUID) -> Bookmark | None:
+        query = {'user_id': str(user_id), 'movie_id': str(movie_id)}
+        document: dict | None = await self.collection.find_one(query)  # type:ignore
+        if document is not None:
+            return Bookmark(**document)
+
+        return None
+
+    async def __count_documents(self, query: dict) -> int:
+        return await self.collection.count_documents(query)
