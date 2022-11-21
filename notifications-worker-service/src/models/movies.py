f19d7a78ffda9c32d561849c4d88986922dfd8d3
kenki <ilsafganiev2222@gmail.com>
2022-11-21 18:41:50 +0300

    Added new models and set orjson as json serialisation for pydantic

--- notifications-worker-service/src/models/movies.py
+++ notifications-worker-service/src/models/movies.py
@@ -3,7 +3,6 @@ import uuid
 from src.models.base import Base
 
 
-class Message(Base):
+class Movie(Base):
     id: uuid.UUID  # noqa: VNE003
-    type: str  # noqa: VNE003
-    content: dict
+    title: str
