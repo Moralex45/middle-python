264e24c0bc789db297d8c5a80fe94f66f02477d9
kenki <ilsafganiev2222@gmail.com>
2022-11-24 21:53:08 +0300

    Implemented handlers

---
+++ notifications-worker-service/src/handlers/__init__.py
@@ -0,0 +1,11 @@
+from protocol import MessageHandlerProtocol, EventHandlerProtocol
+from message_handler import PersonalizedMessageHandler
+from event_handler import EventHandler
+
+
+__all__ = [
+    'MessageHandlerProtocol',
+    'EventHandler',
+    'EventHandlerProtocol',
+    'PersonalizedMessageHandler',
+]
