e7147f6f40413df950a0b1c85b8e1908a74ea3f7
kenki <ilsafganiev2222@gmail.com>
2022-11-24 21:50:00 +0300

    Added new model in broker.py

--- notifications-worker-service/src/models/broker.py
+++ notifications-worker-service/src/models/broker.py
@@ -1,9 +1,17 @@
 import uuid
 
 from src.models.base import Base
+from src.core.settings import EventTypes
 
 
-class Message(Base):
+class EventMessage(Base):
     id: uuid.UUID  # noqa: VNE003
-    type: str  # noqa: VNE003
+    type: EventTypes  # noqa: VNE003
+    content: dict
+
+
+class PersonalizedMessage(Base):
+    id: uuid.UUID  # noqa: VNE003
+    type: EventTypes  # noqa: VNE003
+    email: str
     content: dict
