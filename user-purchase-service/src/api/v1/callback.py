65c8373a4308c763650ecc5d59cd7c6d9c9fbc9a
Aleksandr Morozov <moralex45@gmail.com>
2022-12-25 13:20:39 +0300

    add /callback endpoint

--- user-purchase-service/src/api/v1/callback.py
+++ user-purchase-service/src/api/v1/callback.py
@@ -1,9 +1,16 @@
 import logging
+from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import (
+    APIRouter,
+    Depends,
+    HTTPException
+)
 from starlette import status
 
+from src.services.payment import PaymentService, get_payment_service
 import src.models.http.callback as http_callback
+from src.api.errors import errors_description
 
 logger = logging.getLogger(__name__)
 
@@ -17,5 +24,17 @@ router = APIRouter(prefix='/api/v1/callback')
              tags=['Callback'])
 async def payment(
     payload: http_callback.CallbackData,
+    payment_service: PaymentService = Depends(get_payment_service)
 ) -> None:
-    ...
+    db_payment = await payment_service.get_user_purchase_by_id(
+        payload.payment_id
+    )
+    if not db_payment:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail=errors_description.no_payment
+        )
+
+    await payment_service.update_payment(
+        payload
+    )
